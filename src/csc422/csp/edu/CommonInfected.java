4fc0e8bf7bd20a65f9eef55fa84ae02f542c99b4
dragonpale1 <64854702+dragonpale1@users.noreply.github.com>
2020-12-04 05:11:55 -0600

    Update CommonInfected.java

--- src/csc422/csp/edu/CommonInfected.java
+++ src/csc422/csp/edu/CommonInfected.java
@@ -1,7 +1,10 @@
 package csc422.csp.edu;
 
-public class CommonInfected extends Zombie {
-
-    // Code here
-
+public class CommonInfect extends Zombie
+{
+    public CommonInfect(int startingHealth, int damageValue)
+    {
+        super(startingHealth, damageValue);
+        type = "common infect";
+    }
 }
