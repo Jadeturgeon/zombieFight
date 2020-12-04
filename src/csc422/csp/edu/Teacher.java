49c63defd7b72d7b5cf27ade250f3df28790b588
dragonpale1 <64854702+dragonpale1@users.noreply.github.com>
2020-12-04 05:14:54 -0600

    Update Teacher.java

--- src/csc422/csp/edu/Teacher.java
+++ src/csc422/csp/edu/Teacher.java
@@ -1,7 +1,10 @@
 package csc422.csp.edu;
 
-public class Teacher extends Survivor {
-
-    // Code here
-
+public class Teacher extends Survivor
+{
+    public Teacher(int startingHealth, int damageValue)
+    {
+        super(startingHealth, damageValue);
+        type = "teacher";
+    }
 }
