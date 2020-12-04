9728c8e7fe7e6be5b19b7f302f4fb43fb5f08bb9
dragonpale1 <64854702+dragonpale1@users.noreply.github.com>
2020-12-04 05:04:52 -0600

    Update Zombie.java

--- src/csc422/csp/edu/Zombie.java
+++ src/csc422/csp/edu/Zombie.java
@@ -1,22 +1,42 @@
-package csc422.csp.edu;
-
 import java.util.ArrayList;
+ 
+//12/4/2020 
+//CSC 422 
+//zombie war application
 
-public class Zombie extends Character {
-
-    @Override
-    public void acquireTargets(ArrayList<Character> targets) {
-
+public abstract class Zombie extends Character
+{
+    public Zombie(int startingHealth, int damageValue)
+    {
+        super(startingHealth, damageValue);
     }
 
-    public static ArrayList<Zombie> createRandomZombies(){
+    public static ArrayList<Character> createRandomZombies() throws Exception
+    {
+        ArrayList<Character> list = new ArrayList<>();
 
-        // Variable for Zombie list
-        ArrayList<Zombie> zombies = new ArrayList<>();
+        //(min + (int) (Math.random() * ((max - min) + 1)))
+        int number = (int) (Math.random() * 13);
+        number += 3;
 
-        // Code here
-        return zombies;
+        while (number >= 0)
+        {
+            //create a random zombie
+            int type = (int) (Math.random() * 2) + 1;
 
+            switch (type)
+            {
+                case 1:
+                    list.add(new Tank(150, 20));
+                    break;
+                case 2:
+                    list.add(new CommonInfect(30, 5));
+                    break;
+                default:
+                    throw new Exception("not implemented");
+            }
+            number--;
+        }
+        return list;
     }
-
 }
