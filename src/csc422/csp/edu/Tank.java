04b855b5e900b490b01df81bdaf46aec97b5927e
dragonpale1 <64854702+dragonpale1@users.noreply.github.com>
2020-12-04 05:09:40 -0600

    Update Tank.java

--- src/csc422/csp/edu/Tank.java
+++ src/csc422/csp/edu/Tank.java
@@ -1,7 +1,10 @@
 package csc422.csp.edu;
 
-public class Tank extends Zombie {
-
-    // Code here
-
+public class Tank extends Zombie
+{
+    public Tank(int startingHealth, int damageValue)
+    {
+        super(startingHealth, damageValue);
+        type = "tank";
+    }
 }
