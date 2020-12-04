eda61f518e382df602c3acdcef3259401e3c44d1
dragonpale1 <64854702+dragonpale1@users.noreply.github.com>
2020-12-04 05:13:49 -0600

    Update Soldier.java

--- src/csc422/csp/edu/Soldier.java
+++ src/csc422/csp/edu/Soldier.java
@@ -1,7 +1,10 @@
 package csc422.csp.edu;
 
-public class Soldier {
-
-    // Code here
-
+public class Soldier extends Survivor
+{
+    public Soldier(int startingHealth, int damageValue)
+    {
+        super(startingHealth, damageValue);
+        type = "soldier";
+    }
 }
